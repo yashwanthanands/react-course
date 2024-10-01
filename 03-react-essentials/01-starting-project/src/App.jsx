87ab24aee131238ea493000b49a77a8a76b349c1
Yashwanth Anand S <yashwanthanands@outlook.com>
2024-10-01 18:23:32 +0530

    Used data.js to add the data for other core concepts

--- 03-react-essentials/01-starting-project/src/App.jsx
+++ 03-react-essentials/01-starting-project/src/App.jsx
@@ -44,6 +44,18 @@ function App() {
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image} />
+              <CoreConcepts
+              title={CORE_CONCEPTS[1].title}
+              description={CORE_CONCEPTS[1].description}
+              image={CORE_CONCEPTS[1].image} />
+              <CoreConcepts
+              title={CORE_CONCEPTS[2].title}
+              description={CORE_CONCEPTS[2].description}
+              image={CORE_CONCEPTS[2].image} />
+              <CoreConcepts
+              title={CORE_CONCEPTS[3].title}
+              description={CORE_CONCEPTS[3].description}
+              image={CORE_CONCEPTS[3].image} />
           </ul>
         </section>
         <h2>Time to get started!</h2>
