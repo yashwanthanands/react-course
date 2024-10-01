fcaf202992c69d38d374dcb59212aaf68d41d5a6
Yashwanth Anand S <yashwanthanands@outlook.com>
2024-10-01 19:43:11 +0530

    Passing the data without using props - Object destructing

--- 03-react-essentials/01-starting-project/src/App.jsx
+++ 03-react-essentials/01-starting-project/src/App.jsx
@@ -22,12 +22,12 @@ function Header() {
   );
 }
 
-function CoreConcepts(props) {
+function CoreConcepts({image, title, description}) {
   return (
     <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   );
 }
