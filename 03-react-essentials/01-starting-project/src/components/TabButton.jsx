5fc20e0a88636a8b88a5c5d22517b869f90911b8
Yashwanth Anand S <yashwanthanands@outlook.com>
2024-10-02 10:19:32 +0530

    Children props destructuring

--- 03-react-essentials/01-starting-project/src/components/TabButton.jsx
+++ 03-react-essentials/01-starting-project/src/components/TabButton.jsx
@@ -1,3 +1,3 @@
-export default function TabButton(props) {
-  return (<li><button>{props.children}</button></li>);
+export default function TabButton({children}) {
+  return (<li><button>{children}</button></li>);
 }
