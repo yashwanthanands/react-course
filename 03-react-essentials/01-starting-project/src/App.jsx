cdd39ad89309ff7840c83134722d0269956694a3
Yashwanth Anand S <yashwanthanands@outlook.com>
2024-10-04 13:47:58 +0530

    useState concept

--- 03-react-essentials/01-starting-project/src/App.jsx
+++ 03-react-essentials/01-starting-project/src/App.jsx
@@ -2,10 +2,14 @@ import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
 import CoreConcepts from "./components/CoreConcepts.jsx";
 import TabButton from "./components/TabButton.jsx";
+import { useState } from "react";
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState('Please click a button')
+
   function handleSelect(selectedButton) {
     //selectedButton => 'jsx', 'components', 'props', 'state'
+    setSelectedTopic(selectedButton);
     console.log("Yash has selected a button", selectedButton)
   }
   return (
@@ -29,7 +33,9 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
+          {selectedTopic}
         </section>
+
       </main>
     </div>
   );
