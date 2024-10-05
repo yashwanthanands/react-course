7a3b6af0b3774d21feec64364aafe1e22ee47295
Yashwanth Anand S <yashwanthanands@outlook.com>
2024-10-05 10:49:12 +0530

    Used events to render data dynamically from data.js

--- 03-react-essentials/01-starting-project/src/App.jsx
+++ 03-react-essentials/01-starting-project/src/App.jsx
@@ -3,9 +3,10 @@ import Header from "./components/Header/Header.jsx";
 import CoreConcepts from "./components/CoreConcepts.jsx";
 import TabButton from "./components/TabButton.jsx";
 import { useState } from "react";
+import { EXAMPLES } from "./data.js"
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('Please click a button')
+  const [selectedTopic, setSelectedTopic] = useState('components')
 
   function handleSelect(selectedButton) {
     //selectedButton => 'jsx', 'components', 'props', 'state'
@@ -33,11 +34,19 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>
+                {EXAMPLES[selectedTopic].code}
+              </code>
+            </pre>
+          </div>
         </section>
 
       </main>
-    </div>
+    </div >
   );
 }
 
